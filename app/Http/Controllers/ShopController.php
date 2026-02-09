ba7d27b96e6c35d33b4481d0caac00989fd1f854
Rost445 <e-mail@fts.ua>
2026-02-09 09:21:17 +0200

    filter

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -7,11 +7,41 @@ use App\Models\Product;
 
 class ShopController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $products = Product::orderBy('created_at', 'DESC')->paginate(12);
+        $size = $request->query('size') ? $request->query('size') : 12;
+        $o_column = "";
+        $o_order = "";
 
-        return view('shop', compact('products'));
+        $order = $request->query('order') ? $request->query('order') : -1;
+       
+        switch ($order) {
+            case 1:
+                $o_column = 'created_at';
+                $o_order = 'DESC';
+                break;
+            case 2:
+                $o_column = 'created_at';
+                $o_order = 'ASC';
+                break;
+                case 3:
+                $o_column = 'regular_price';
+                $o_order = 'ASC';
+                break;
+                case 4:
+                $o_column = 'regular_price';
+                $o_order = 'DESC';
+                break;
+            default:
+                $o_column = 'id';
+                $o_order = 'DESC';
+                break;
+        }
+
+        $products = Product::orderBy($o_column, $o_order)
+            ->paginate($size);
+
+        return view('shop', compact('products', 'size', 'order'));
     }
 
     public function product_details($product_slug)
--- resources/views/shop.blade.php
+++ resources/views/shop.blade.php
@@ -367,16 +367,23 @@
                     <div
                         class="shop-acs d-flex align-items-center justify-content-between justify-content-md-end flex-grow-1">
                         <select class="shop-acs__select form-select w-auto border-0 py-0 order-1 order-md-0"
-                            aria-label="Sort Items" name="total-number">
-                            <option selected>Default Sorting</option>
-                            <option value="1">Featured</option>
-                            <option value="2">Best selling</option>
-                            <option value="3">Alphabetically, A-Z</option>
-                            <option value="3">Alphabetically, Z-A</option>
-                            <option value="3">Price, low to high</option>
-                            <option value="3">Price, high to low</option>
-                            <option value="3">Date, old to new</option>
-                            <option value="3">Date, new to old</option>
+                            aria-label="Page Size" id="pagesize" name="pagesize" style="margin-right: 20px">
+                            <option value="12" {{ $size == 12 ? 'selected' : '' }}>Показати</option>
+                            <option value="24" {{ $size == 24 ? 'selected' : '' }}>Показати 24</option>
+                            <option value="48" {{ $size == 48 ? 'selected' : '' }}>Показати 48</option>
+                            <option value="102" {{ $size == 102 ? 'selected' : '' }}>Показати 102</option>
+                        </select>
+
+                        <select class="shop-acs__select form-select w-auto border-0 py-0 order-1 order-md-0"
+                            aria-label="Сортування товарів" name="orderby" id="orderby">
+                            <option value="-1" {{ $order == -1 ? 'selected' : '' }}>За замовчуванням</option>
+                            <option value="1" {{ $order == 1 ? 'selected' : '' }}>Дата: від нових до старих</option>
+                            <option value="2" {{ $order == 2 ? 'selected' : '' }}>Дата: від старих до нових</option>
+                            <option value="3" {{ $order == 3 ? 'selected' : '' }}>Ціна: від дешевих до дорогих</option>
+                            <option value="4" {{ $order == 4 ? 'selected' : '' }}>Ціна: від дорогих до дешевих
+                            </option>
+
+
                         </select>
 
                         <div class="shop-asc__seprator mx-3 bg-light d-none d-md-block order-md-0"></div>
@@ -460,7 +467,8 @@
                                                 <input type="hidden" name="name" value="{{ $product->name }}" />
                                                 <input type="hidden" name="price"
                                                     value="{{ $product->sale_price == '' ? $product->regular_price : $product->sale_price }}" /><!-- .qty-control -->
-                                                <button type="submit" class="pc__atc btn anim_appear-bottom btn position-absolute border-0 text-uppercase fw-medium"
+                                                <button type="submit"
+                                                    class="pc__atc btn anim_appear-bottom btn position-absolute border-0 text-uppercase fw-medium"
                                                     data-aside="cartDrawer">Додати до кошика</button>
                                             </div>
                                         </form>
@@ -480,9 +488,9 @@
                                                     &nbsp;₴</s> {{ $product->sale_price }} &nbsp;₴
                                             @else
                                                 <span class="price-old text-secondary text-decoration-line-through ms-2">
-                                                    ${{ number_format($product->price, 2) }}
+                                                    ${{ number_format($product->regular_price, 2) }}
                                                 </span>
-                                                {{ number_format($product->price, 2) }} &nbsp;₴
+                                                {{ number_format($product->regular_price, 2) }} &nbsp;₴
                                             @endif
                                         </span>
                                     </div>
@@ -528,9 +536,37 @@
 
                 <div class="divider"></div>
                 <div class="flex items-center justify-between flex-wrap gap10 wgp-pagination">
-                    {{ $products->links('pagination::bootstrap-5') }}
+                    {{ $products->withQueryString()->links('pagination::bootstrap-5') }}
                 </div>
             </div>
         </section>
     </main>
+
+
+
+    <form id="frmfilters" method="get" action="{{ route('shop.index') }}">
+        @csrf
+
+        <input type="hidden" name="page" value="{{ $products->currentPage() }}">
+        <input type="hidden" name="size" id="size" value="{{ $size }}">
+        <input type="hidden" name="order" id="order" value="{{ $order }}">
+    </form>
 @endsection
+
+@push('scripts')
+    <script>
+        $(function() {
+            $("#pagesize").on("change", function() {
+                $("#size").val($("#pagesize option:selected").val());
+                $("#frmfilters").submit();
+
+            });
+
+            $("#orderby").on("change", function() {
+                $("#order").val($("#orderby option:selected").val());
+                $("#frmfilters").submit();
+
+            });
+        });
+    </script>
+    @endpush

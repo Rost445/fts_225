cfb741cb13936a20465060c60b46e5561a2963fe
Rost445 <e-mail@fts.ua>
2026-02-19 11:16:58 +0200

    Show All Wishlisted Products

--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,7 @@ Route::delete('/cart/remove/{rowId}', [CartController::class, 'remove_item'])->n
 Route::delete('/cart/empty', [CartController::class, 'empty_cart'])->name('cart.empty');
 
 Route::post('/wishlist/add', [WishlistController::class, 'add_to_wishlist'])->name('wishlist.add');
+Route::get('/wishlist', [WishlistController::class, 'index'])->name('wishlist.index');
 
 
 

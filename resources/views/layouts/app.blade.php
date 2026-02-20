718022461ef8c6423689fab3f8eb4b9f9a1d6abd
Rost445 <e-mail@fts.ua>
2026-02-20 08:54:02 +0200

    деякі правки по кошику

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -278,7 +278,7 @@
                 </a>
             </div>
 
-            <a href="#" class="header-tools__item header-tools__cart js-open-aside" data-aside="cartDrawer">
+            <a href="{{ route('cart.index') }}" class="header-tools__item header-tools__cart" >
                 <svg class="d-block" width="20" height="20" viewBox="0 0 20 20" fill="none"
                     xmlns="http://www.w3.org/2000/svg">
                     <use href="#icon_cart" />
@@ -497,7 +497,7 @@
                         @if (Cart::instance('wishlist')->content()->count() > 0)
                             <span
                                 class="cart-amount d-block position-absolute js-cart-items-count">{{ Cart::instance('wishlist')->content()->count() }}
-                              </span>
+                            </span>
                         @endif
                     </a>
 
@@ -657,35 +657,43 @@
     <footer class="footer-mobile container w-100 px-5 d-md-none bg-body">
         <div class="row text-center">
             <div class="col-4">
-                <a href="index.html" class="footer-mobile__link d-flex flex-column align-items-center">
+                <a href="{{ route('home.index') }}"
+                    class="footer-mobile__link d-flex flex-column align-items-center">
                     <svg class="d-block" width="18" height="18" viewBox="0 0 18 18" fill="none"
                         xmlns="http://www.w3.org/2000/svg">
                         <use href="#icon_home" />
                     </svg>
-                    <span>Home</span>
+                    <span>Головна</span>
                 </a>
             </div>
 
             <div class="col-4">
-                <a href="index.html" class="footer-mobile__link d-flex flex-column align-items-center">
+                <a href="{{ route('shop.index') }}"
+                    class="footer-mobile__link d-flex flex-column align-items-center">
                     <svg class="d-block" width="18" height="18" viewBox="0 0 18 18" fill="none"
                         xmlns="http://www.w3.org/2000/svg">
                         <use href="#icon_hanger" />
                     </svg>
-                    <span>Shop</span>
+                    <span>Магазин</span>
                 </a>
             </div>
 
             <div class="col-4">
-                <a href="index.html" class="footer-mobile__link d-flex flex-column align-items-center">
+
+                <a href="{{ route('wishlist.index') }}"
+                    class="footer-mobile__link d-flex flex-column align-items-center">
                     <div class="position-relative">
                         <svg class="d-block" width="18" height="18" viewBox="0 0 20 20" fill="none"
                             xmlns="http://www.w3.org/2000/svg">
                             <use href="#icon_heart" />
                         </svg>
-                        <span class="wishlist-amount d-block position-absolute js-wishlist-count">3</span>
+                        @if (Cart::instance('wishlist')->content()->count() > 0)
+                            <span
+                                class="wishlist-amount d-block position-absolute
+                         js-wishlist-count">{{ Cart::instance('wishlist')->content()->count() }}</span>
                     </div>
                     <span>Список бажань</span>
+                    @endif
                 </a>
             </div>
         </div>
